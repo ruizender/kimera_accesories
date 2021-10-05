f978fb11380089a822b1f348f7492d8e3852e7df
Ender Ruiz <ruiz.valera@gmail.com>
2021-10-04 22:36:09 -0300

    add relationships to tables

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :shopping_carts, dependent: :destroy
+  has_many :products, through: :shopping_carts, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :shopping_carts, dependent: :destroy
+  has_many :orders, through: :shopping_carts, dependent: :destroy
 end
