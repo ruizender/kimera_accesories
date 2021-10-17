f4d397399d229c49daef4687e259738a8179077b
Ender Ruiz <ruiz.valera@gmail.com>
2021-10-16 23:49:17 -0300

    add gem ratyrate

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,17 +3,12 @@ class Product < ApplicationRecord
   has_many :shopping_carts, dependent: :destroy
   has_many :orders, through: :shopping_carts, dependent: :destroy
   validates :name, presence: true
-
+  ratyrate_rateable "price"
 
 
   def to_s
     name
   end
-
-
-
-
-
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_040504) do
+ActiveRecord::Schema.define(version: 2021_10_16_225354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_10_14_040504) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "average_caches", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "avg", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -56,6 +65,14 @@ ActiveRecord::Schema.define(version: 2021_10_14_040504) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "overall_averages", id: :serial, force: :cascade do |t|
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "overall_avg", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "sku"
@@ -67,6 +84,30 @@ ActiveRecord::Schema.define(version: 2021_10_14_040504) do
     t.datetime "updated_at", null: false
     t.integer "price"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "rates", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "stars", null: false
+    t.string "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.bigint "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
   end
 
   create_table "shopping_carts", force: :cascade do |t|
