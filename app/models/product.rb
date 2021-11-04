db73827e9e0995c884cdc11cf02e2b59735e5e4a
Ender Ruiz <ruiz.valera@gmail.com>
2021-11-04 02:37:38 -0300

    modify views show products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :shopping_carts, dependent: :destroy
   has_many :orders, through: :shopping_carts, dependent: :destroy
-  validates :name, :sku, :descriptions, :stock, :price, presence: true
+  validates :name, :sku, :descriptions, :stock, :price, :pictures, presence: true
   ratyrate_rateable "price"
   has_many_attached :pictures
 
